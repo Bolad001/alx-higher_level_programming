0bb2761c923c10b9ad37fbe50d2ef18eff3c30c2
Abubakar <adebayoabubakar41@gmail.com>
2022-08-27 12:46:39 -0700

    commit task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+
+    resultAll = 0
+    for a in range(1, len(argv)):
+        resultAll = resultAll + int(argv[a])
+    print(resultAll)
