e3092a7bd03c6c8a3f0e62441f11ff4ecbb7f3b7
Abubakar <adebayoabubakar41@gmail.com>
2022-08-30 11:01:18 -0700

    commit task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
+    maxInt = my_list[0]
     for i in my_list:
-        if i > max:
-            max = i
-    return max
+        if i > maxInt:
+            maxInt = i
+    return maxInt
     return None
